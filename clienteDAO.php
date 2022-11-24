1bb12c1a504863286d3f8d74e7fc1da9604b300e
Bruno Ricardo Carmena <106131265+brunocarmena57@users.noreply.github.com>
2022-11-24 14:47:29 -0300

    Update clienteDAO.php

--- clienteDAO.php
+++ clienteDAO.php
@@ -5,12 +5,14 @@ function conectaDB(){
     return new PDO("mysql:host=localhost;dbname=aulaphpbrunor", "root", "");
 }
 
+// Inserta os valores no banco de dados table clientes
 function cadClienteDAO($cliente){
     $pdo = conectaDB();
     $sql = $pdo->prepare("INSERT INTO clientes VALUES(null, ?,?,?,?)");
     $sql->execute(array_values($cliente));
 }
 
+// Função alterar clientes
 function altClienteDAO($cliente){
     $pdo = conectaDB();
     $pdo->exec('UPDATE clientes SET
@@ -22,20 +24,22 @@ function altClienteDAO($cliente){
     );
 }
 
+// Função deletar clientes
 function delClienteDAO($id){
     $pdo = conectaDB();
     $pdo->exec("DELETE FROM clientes WHERE id=$id");
     echo "Clientes deletado com sucesso";
 }
 
+//Função listar Clientes
 function listarClientesDAO(){
     $pdo = conectaDB();
-    
     $sql = $pdo->prepare("SELECT * FROM clientes");
     $sql->execute();
     return $sql->fetchAll(PDO::FETCH_ASSOC);
 }
 
+//Função buscar clientes
 function buscarClienteDAO($id){
     $pdo = conectaDB();
     $sql = $pdo->prepare("SELECT * FROM clientes WHERE id= ?");
